015d3d9a1656e6f291a53847c1aa1fe0db8485a2
HuynhThiKhanhLinh <bongudth@gmail.com>
2021-05-27 23:48:21 +0700

    Update heroku

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-import request from "request"
+import request from "request";
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
@@ -100,7 +100,7 @@ function callSendAPI(sender_psid, response) {
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
@@ -113,7 +113,7 @@ function callSendAPI(sender_psid, response) {
 }
 
 module.exports = {
-  getHomePage: getHomePage, //key: value
+  getHomePage: getHomePage,
   getWebhook: getWebhook,
   postWebhook: postWebhook,
 };
